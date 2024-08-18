933a9c22269efa6914d27204306115e489a1472d
René DeAnda <rene.ideanda@gmail.com>
2024-08-18 09:18:21 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-
 'use client';
 
+import React, { Suspense } from 'react';
 import BookRecommendationApp from '@/components/BookRecommendationApp';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-      <BookRecommendationApp />
+      <Suspense fallback={<div>Loading application...</div>}>
+        <BookRecommendationApp />
+      </Suspense>
     </main>
   );
 }
