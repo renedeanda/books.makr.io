4c1aa70c434cdd80ce8271f8c13d6973689a4862
René DeAnda <rene.ideanda@gmail.com>
2024-08-18 14:01:40 -0700

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
       <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-8">Oops! The page you\'re looking for doesn\'t exist.</p>
+      <p className="text-xl mb-8">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link href="/" className="text-blue-500 hover:underline">
         Go back to homepage
       </Link>
